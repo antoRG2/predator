591c3c4f9625d2be66e01a11e1a4ecb0af4725bc
antoRG2 <anto_rg24@hotmail.com>
2015-07-12 17:24:04 -0600

    js changes: adding objects definition

--- main.js
+++ main.js
@@ -6,3 +6,68 @@ window.onload = function () {
     content.innerHTML = 'Content';
 
 }
+
+
+//Personal Info Object
+var personalInformation = new Object();
+
+personalInformation.name = "";
+personalInformation.photo = "";
+personalInformation.country = "";
+personalInformation.mail = "";
+personalInformation.linkedin = "";
+personalInformation.skype = "";
+
+//Knowledge Object
+var knowledge = {};
+
+knowledge["description"] = "";
+knowledge["yearsExperience"] = "";
+
+//cover letter Object
+var coverLetter = Object.create(null);
+
+Object.defineProperty(coverLetter, "text", {
+    value: "",
+    writable: true,
+    enumerable: true,
+    configurable:true
+ });
+Object.defineProperty(coverLetter, "signature", {
+    value: "",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+Object.defineProperty(coverLetter, "date", {
+    value: "",
+    writable: true,
+    enumerable: true,
+    configurable:true
+});
+
+
+//experience Object
+var experience = {
+        companyName: "",
+        positionName: "",
+        yearStart: "",
+        yearEnd: ""
+    }
+
+
+//projects Object
+function Projects() {
+
+    this.name= "";
+    this.description= "";
+    this.previewImage= "";
+}
+
+var project = new Projects();
+
+
+//hobbies Object
+var hobbies = new Object();
+
+hobbies["description"] = "";
